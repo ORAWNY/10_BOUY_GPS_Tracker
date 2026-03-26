49b2c0cd498b2f60d46c63a2634cfaea96431945
ORAWNY <odhran.mccarthy@greenrebel.ie>
2026-03-26 14:09:01 +0000

    Echowende_production
    Current working version of Hydraparse for Echowende_26032026

--- Launch_hydraprase_GRDATA2.bat
+++ Launch_hydraprase_GRDATA2.bat
@@ -1,9 +1,9 @@
-@echo off
+@echo on
 setlocal
 
 REM --- ADJUSTED PATHS (keep the quotes) ---
-set "PYTHON_EXE=D:\01_HydraParse\10_BOUY_GPS_Tracker\.venv\Scripts\pythonw.exe"
-set "APP_DIR=D:\01_HydraParse\10_BOUY_GPS_Tracker"
+set "PYTHON_EXE=C:\Users\GRData\OneDrive - GREEN REBEL GROUP\Desktop\Odhran\10_BOUY_GPS_Tracker\.venv\Scripts\pythonw.exe"
+set "APP_DIR=C:\Users\GRData\OneDrive - GREEN REBEL GROUP\Desktop\Odhran\10_BOUY_GPS_Tracker"
 
 REM --- go to the app folder so relative paths work ---
 cd /d "%APP_DIR%"
